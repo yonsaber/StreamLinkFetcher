5e329e5611b2a64df4b82c1987ecb9c4e385e06f
riley <riley@hidinginthevoid.com>
2020-07-09 22:00:54 +0100

    Initial commit with the basic application

---
+++ StreamLinkFetcher/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace StreamLinkFetcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_TwitchLink = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_GetStreamUrl = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.txt_StreamVideoLink = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_CopyToClipboard = new System.Windows.Forms.Button();
+            this.lst_Diag = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmb_RingBuffer = new System.Windows.Forms.ComboBox();
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_TwitchLink
+            // 
+            this.txt_TwitchLink.Location = new System.Drawing.Point(15, 25);
+            this.txt_TwitchLink.Name = "txt_TwitchLink";
+            this.txt_TwitchLink.Size = new System.Drawing.Size(313, 20);
+            this.txt_TwitchLink.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Stream URL / DJ Twitch Name";
+            // 
+            // btn_GetStreamUrl
+            // 
+            this.btn_GetStreamUrl.Location = new System.Drawing.Point(15, 92);
+            this.btn_GetStreamUrl.Name = "btn_GetStreamUrl";
+            this.btn_GetStreamUrl.Size = new System.Drawing.Size(152, 23);
+            this.btn_GetStreamUrl.TabIndex = 2;
+            this.btn_GetStreamUrl.Text = "Get Stream URL";
+            this.btn_GetStreamUrl.UseVisualStyleBackColor = true;
+            this.btn_GetStreamUrl.Click += new System.EventHandler(this.btn_GetStreamUrl_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(173, 92);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(155, 23);
+            this.btn_Clear.TabIndex = 3;
+            this.btn_Clear.Text = "Clear Input";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // txt_StreamVideoLink
+            // 
+            this.txt_StreamVideoLink.Enabled = false;
+            this.txt_StreamVideoLink.Location = new System.Drawing.Point(15, 169);
+            this.txt_StreamVideoLink.Name = "txt_StreamVideoLink";
+            this.txt_StreamVideoLink.Size = new System.Drawing.Size(313, 20);
+            this.txt_StreamVideoLink.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Stream Video Link";
+            // 
+            // btn_CopyToClipboard
+            // 
+            this.btn_CopyToClipboard.Location = new System.Drawing.Point(15, 195);
+            this.btn_CopyToClipboard.Name = "btn_CopyToClipboard";
+            this.btn_CopyToClipboard.Size = new System.Drawing.Size(313, 23);
+            this.btn_CopyToClipboard.TabIndex = 6;
+            this.btn_CopyToClipboard.Text = "Copy To Clipboard";
+            this.btn_CopyToClipboard.UseVisualStyleBackColor = true;
+            this.btn_CopyToClipboard.Click += new System.EventHandler(this.btn_CopyToClipboard_Click);
+            // 
+            // lst_Diag
+            // 
+            this.lst_Diag.Enabled = false;
+            this.lst_Diag.Location = new System.Drawing.Point(15, 239);
+            this.lst_Diag.Multiline = true;
+            this.lst_Diag.Name = "lst_Diag";
+            this.lst_Diag.Size = new System.Drawing.Size(313, 258);
+            this.lst_Diag.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "RingBuffer Size";
+            // 
+            // cmb_RingBuffer
+            // 
+            this.cmb_RingBuffer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_RingBuffer.FormattingEnabled = true;
+            this.cmb_RingBuffer.Location = new System.Drawing.Point(15, 65);
+            this.cmb_RingBuffer.Name = "cmb_RingBuffer";
+            this.cmb_RingBuffer.Size = new System.Drawing.Size(313, 21);
+            this.cmb_RingBuffer.TabIndex = 9;
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(StreamLinkFetcher.Program);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_GetStreamUrl;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 509);
+            this.Controls.Add(this.cmb_RingBuffer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lst_Diag);
+            this.Controls.Add(this.btn_CopyToClipboard);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_StreamVideoLink);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_GetStreamUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_TwitchLink);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Stream Extractor";
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_TwitchLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_GetStreamUrl;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.TextBox txt_StreamVideoLink;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_CopyToClipboard;
+        private System.Windows.Forms.TextBox lst_Diag;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmb_RingBuffer;
+        private System.Windows.Forms.BindingSource programBindingSource;
+    }
+}
+
---
+++ StreamLinkFetcher/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StreamLinkFetcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btn_CopyToClipboard.Enabled = false;
+            Text += $" {Assembly.GetExecutingAssembly().GetName().Version}";
+
+            // I really don't know what this does but makes things easier
+            var ringBufferSizes = new RingBufferSize[]
+            {
+                new RingBufferSize{ Text = "128K" },
+                new RingBufferSize{ Text = "256K" },
+                new RingBufferSize{ Text = "512K" },
+                new RingBufferSize{ Text = "1M" },
+                new RingBufferSize{ Text = "2M" },
+                new RingBufferSize{ Text = "4M" },
+                new RingBufferSize{ Text = "8M" },
+                new RingBufferSize{ Text = "16M" }
+            };
+
+            cmb_RingBuffer.ValueMember = "ID";
+            cmb_RingBuffer.DisplayMember = "Text";
+            cmb_RingBuffer.DataSource = ringBufferSizes;
+            cmb_RingBuffer.SelectedIndex = ringBufferSizes.Length - 1;
+        }
+
+        private async void btn_GetStreamUrl_Click(object sender, EventArgs e)
+        {
+            txt_StreamVideoLink.Text = "";
+
+            try
+            {
+                if (string.IsNullOrEmpty(txt_TwitchLink.Text))
+                    return;
+
+                string streamUrl = txt_TwitchLink.Text;
+
+                // We don't want to accidentally append things!
+                if (!streamUrl.Contains("https://") && streamUrl.IndexOf("twitch.tv", 0, StringComparison.InvariantCultureIgnoreCase) >= 0)
+                {
+                    streamUrl = $"https://{streamUrl}";
+                }
+                else if (!streamUrl.Contains("https://") && streamUrl.IndexOf("twitch.tv", 0, StringComparison.InvariantCultureIgnoreCase) == -1)
+                {
+                    streamUrl = $"https://twitch.tv/{streamUrl}";
+                }
+
+                txt_TwitchLink.Enabled = false;
+                btn_GetStreamUrl.Enabled = false;
+                btn_Clear.Enabled = false;
+
+                lst_Diag.AppendText($"Fetching video url for {streamUrl}... Please wait{Environment.NewLine}");
+
+                RingBufferSize bufferSize = (RingBufferSize)cmb_RingBuffer.SelectedItem;
+
+                // Await so we don't do things we don't want happening before we have the output
+                await Task.Run(() => BuildCommand(streamUrl, bufferSize.Text));
+                var fetchedUrl = Clipboard.GetText().Trim();
+
+                if (fetchedUrl != null)
+                {
+                    Clipboard.SetText(fetchedUrl);
+                    if (fetchedUrl.Contains("error"))
+                    {
+                        lst_Diag.AppendText($"Fetch Failed with message: {fetchedUrl}{Environment.NewLine}");
+                    }
+                    else if (!fetchedUrl.Contains("https://") || !fetchedUrl.Contains("m3u8"))
+                    {
+                        lst_Diag.AppendText($"We didn't get back a URL, are you sure streamlink is installed??{Environment.NewLine}");
+                    }
+                    else
+                    {
+                        txt_StreamVideoLink.Text = fetchedUrl;
+                        lst_Diag.AppendText($"Fetched Successfully!{Environment.NewLine}");
+                        btn_CopyToClipboard.Enabled = true;
+                    }
+                }
+
+                txt_TwitchLink.Enabled = true;
+                btn_GetStreamUrl.Enabled = true;
+                btn_Clear.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                lst_Diag.AppendText($"Contact riley... Something went wrong! You can try again but something did happen :( {ex.Message}{Environment.NewLine}");
+                WriteToFile($"{ex.Message}\n\n{ex.StackTrace}");
+            }
+
+            lst_Diag.AppendText("\n");
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_TwitchLink.Clear();
+            txt_StreamVideoLink.Clear();
+
+            WriteLogToFile();
+
+            lst_Diag.Clear();
+
+            GC.Collect();
+
+            btn_CopyToClipboard.Enabled = false;
+        }
+
+        private void btn_CopyToClipboard_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txt_StreamVideoLink.Text);
+        }
+
+        void WriteLogToFile()
+        {
+            WriteToFile(lst_Diag.Text);
+        }
+
+        void WriteToFile(string data)
+        {
+            File.AppendAllText(@".\\StreamExtractorLog.txt", $"{DateTime.Now}{Environment.NewLine}----{Environment.NewLine}{data}----{Environment.NewLine}{Environment.NewLine}");
+        }
+
+        void BuildCommand(string streamLink, string ringBufferSize)
+        {
+            // TODO (riley): Implement this
+            // If we call streamlink {streamLink} --json we can get back all the sizes of video
+            // We can mod this call with --json to get data back as a JSON object!
+            string command = $"streamlink {streamLink} best --stream-url --ringbuffer-size {ringBufferSize} | clip";
+
+            Invoke(
+                new MethodInvoker(
+                    delegate ()
+                    {
+                        lst_Diag.AppendText($"Running command: {command}{Environment.NewLine}");
+                    }
+                )
+            );
+
+            // Call out to StreamLink
+            RunCommand(command);
+        }
+
+        void RunCommand(string command)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "cmd.exe",
+                Arguments = $"/C {command}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            process.StartInfo = startInfo;
+            process.Start();
+            while (!process.StandardOutput.EndOfStream)
+            {
+                Debug.WriteLine(process.StandardOutput.ReadLine());
+            }
+            process.WaitForExit();
+
+            GC.Collect();
+        }
+    }
+
+    public class RingBufferSize
+    {
+        public string Text { get; set; }
+    }
+}
