2df2184e814c67147f04d8e0dffbc143f25eaadb
riley <riley@hidinginthevoid.com>
2020-09-29 20:51:12 +0100

    Pick up StreamLink protable or CLI versions

--- StreamLinkFetcher/Form1.cs
+++ StreamLinkFetcher/Form1.cs
@@ -7,13 +7,43 @@ using System.Windows.Forms;
 
 namespace StreamLinkFetcher
 {
+    /*
+     * Note: I am super aware this code is messy and doesn't show my BEST work
+     *       but I wrote this for a one off event and have done a handful of tiny
+     *       patches to it for the Unison Team. One day I might actually clean it up
+     *       and make it cross platform etc, but for now it works...
+     */
     public partial class Form1 : Form
     {
+        StreamLinkType slType = StreamLinkType.NONE;
+        bool isNested = false;
+
         public Form1()
         {
             InitializeComponent();
             btn_CopyToClipboard.Enabled = false;
             Text += $" {Assembly.GetExecutingAssembly().GetName().Version}";
+
+            slType = CheckForStreamLinkType();
+
+            if (slType == StreamLinkType.NONE)
+            {
+                // Safety
+                txt_TwitchLink.Enabled = false;
+                cmb_RingBuffer.Enabled = false;
+                txt_StreamVideoLink.Enabled = false;
+                btn_GetStreamUrl.Enabled = false;
+                btn_Clear.Enabled = false;
+
+                // Go install streamlink
+                MessageBox.Show(
+                    $"We haven't detected Streamlink on this system, please use either the portable version or the CLI version.{Environment.NewLine}Application will close when you click OK",
+                    "Missing StreamLink",
+                    MessageBoxButtons.OK);
+
+                // Bye bye
+                Environment.Exit(1);
+            }
 
             // I really don't know what this does but makes things easier
             var ringBufferSizes = new RingBufferSize[]
@@ -85,15 +115,18 @@ namespace StreamLinkFetcher
                         btn_CopyToClipboard.Enabled = true;
                     }
                 }
-
-                txt_TwitchLink.Enabled = true;
-                btn_GetStreamUrl.Enabled = true;
-                btn_Clear.Enabled = true;
             }
             catch (Exception ex)
             {
                 lst_Diag.AppendText($"Contact riley... Something went wrong! You can try again but something did happen :( {ex.Message}{Environment.NewLine}");
                 WriteToFile($"{ex.Message}\n\n{ex.StackTrace}");
+            }
+            finally
+            {
+                // Re-enable cause...Might be a fluke it didn't work so allow for re-tries
+                txt_TwitchLink.Enabled = true;
+                btn_GetStreamUrl.Enabled = true;
+                btn_Clear.Enabled = true;
             }
 
             lst_Diag.AppendText("\n");
@@ -133,7 +166,26 @@ namespace StreamLinkFetcher
             // TODO (riley): Implement this
             // If we call streamlink {streamLink} --json we can get back all the sizes of video
             // We can mod this call with --json to get data back as a JSON object!
-            string command = $"streamlink {streamLink} best --stream-url --ringbuffer-size {ringBufferSize} | clip";
+            
+            string streamLinkExe = "";
+
+            if (slType == StreamLinkType.PORTABLE)
+            {
+                if (isNested)
+                {
+                    streamLinkExe = "streamlink\\streamlink.bat";
+                }
+                else
+                {
+                    streamLinkExe = "streamlink.bat";
+                }
+            }
+            else if (slType == StreamLinkType.CLI)
+            {
+                streamLinkExe = "streamlink";
+            }
+
+            string command = $"{streamLinkExe} {streamLink} best --stream-url --ringbuffer-size {ringBufferSize} | clip";
 
             Invoke(
                 new MethodInvoker(
@@ -171,10 +223,63 @@ namespace StreamLinkFetcher
 
             GC.Collect();
         }
+
+        StreamLinkType CheckForStreamLinkType()
+        {
+            var currentLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (File.Exists(Path.Combine(currentLocation, "Streamlink.exe")) || File.Exists(Path.Combine(currentLocation, "Streamlink.bat")))
+            {
+                return StreamLinkType.PORTABLE;
+            }
+
+            if (
+                File.Exists(Path.Combine(currentLocation, "Streamlink", "Streamlink.exe")) ||
+                File.Exists(Path.Combine(currentLocation, "Streamlink", "Streamlink.bat"))
+            )
+            {
+                isNested = true;
+                return StreamLinkType.PORTABLE;
+            }
+
+            if (ExistsOnPath("streamlink.exe"))
+            {
+                return StreamLinkType.CLI;
+            }
+
+            return StreamLinkType.NONE;
+        }
+
+        // https://stackoverflow.com/a/3856090 StackOverflow is so good sometimes
+        public static bool ExistsOnPath(string fileName)
+        {
+            return GetFullPath(fileName) != null;
+        }
+
+        public static string GetFullPath(string fileName)
+        {
+            if (File.Exists(fileName))
+                return Path.GetFullPath(fileName);
+
+            var values = Environment.GetEnvironmentVariable("PATH");
+            foreach (var path in values.Split(Path.PathSeparator))
+            {
+                var fullPath = Path.Combine(path, fileName);
+                if (File.Exists(fullPath))
+                    return fullPath;
+            }
+            return null;
+        }
     }
 
     public class RingBufferSize
     {
         public string Text { get; set; }
+    }
+
+    public enum StreamLinkType
+    {
+        CLI,
+        PORTABLE,
+        NONE
     }
 }
